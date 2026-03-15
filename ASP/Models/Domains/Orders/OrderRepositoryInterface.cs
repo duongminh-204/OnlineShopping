2cdf659e875ec54f7dacf52792f5a4d3ab7fdf71
DESKTOP-C3NUS3R\Asus <longx3732@gmail.com>
2026-03-15 09:50:04 +0700

    feature(admin) : Add Order management

--- ASP/Models/Domains/Orders/OrderRepositoryInterface.cs
+++ ASP/Models/Domains/Orders/OrderRepositoryInterface.cs
@@ -1,6 +1,21 @@
-﻿namespace ASP.Models.Domains
+using ReflectionIT.Mvc.Paging;
+using System;
+using System.Threading.Tasks;
+using ASP.Models.ViewModels.Orders;
+using ASP.Models.ViewModels.OrderDetails;
+
+namespace ASP.Models.Domains
 {
     public interface OrderRepositoryInterface
     {
+        Task<PagingList<OrderListItem>> GetAllByFilterAsync(
+            string? status,
+            DateTime? fromDate,
+            DateTime? toDate,
+            int pageSize,
+            int page,
+            string? sort);
+            
+        Task<OrderDetailViewModel?> GetByIdAsync(int orderId);
     }
 }
